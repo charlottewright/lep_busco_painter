993e76e39fec47a66a98c01cd2f52ccb14fdcb89
Charlotte Wright <57258050+charlottewright@users.noreply.github.com>
2022-09-20 15:40:12 +0100

    buscopainter.py now reports status by default in locations.tsv

--- buscopainter.py
+++ buscopainter.py
@@ -21,6 +21,7 @@ def print_summary_table(reference_table_dict, reference_chr_list, query_table_di
 	with open(summary_table_file, 'w') as summary_table:
 		summary_table.write("%s\t%s\t%s\t%s\t%s\t%s" % ("query_chr", "assigned_chr", "assigned_chr_busco_count", "total_query_chr_busco_chr", "perc", '\t'.join("ref_" + (x) + "_count" for x in reference_chr_list)) + "\n")
 		assignment_dict = {}
+		top_chr_dict = {}
 		for chr in query_chr_list:
 			assignment_dict[chr] = []
 			for i in range(0, len(reference_chr_list)):
@@ -45,16 +46,22 @@ def print_summary_table(reference_table_dict, reference_chr_list, query_table_di
 					top_chr, top_chr_count = reference_chr, count
 			perc = round((top_chr_count / total), 2)
 			summary_table.write("%s\t%s\t%s\t%s\t%s\t%s" % (chr, top_chr, top_chr_count, total, perc, '\t'.join(str(x) for x in count_list)) + "\n")
+			top_chr_dict[chr] = top_chr
+	return top_chr_dict
 
-def print_location_table(reference_table_dict, query_table_dict, location_table_file):
+def print_location_table(reference_table_dict, query_table_dict, location_table_file, top_chr_dict):
 	with open(location_table_file, 'w') as location_table:
-		location_table.write("%s\t%s\t%s\t%s" % ("buscoID", "query_chr", "position", "assigned_chr") + "\n")
+		location_table.write("%s\t%s\t%s\t%s\t%s" % ("buscoID", "query_chr", "position", "assigned_chr", "status") + "\n")
 		for buscoID, position_list in query_table_dict.items():
 			query_chr, query_start, query_stop = position_list
 			position = (query_start + query_stop)/2
 			try:
 				reference_chr = reference_table_dict[buscoID][0]
-				location_table.write("%s\t%s\t%s\t%s" % (buscoID, query_chr, position, reference_chr) + "\n")
+				if reference_chr != top_chr_dict[query_chr]:
+					status = reference_chr
+				else:
+					status = "self"
+				location_table.write("%s\t%s\t%s\t%s\t%s" % (buscoID, query_chr, position, reference_chr, status) + "\n")
 			except KeyError:
 				pass
 
@@ -74,8 +81,8 @@ if __name__ == "__main__":
 	# run the functions
 	reference_table_dict, reference_chr_list = parse_table(reference_table_file)
 	query_table_dict, query_chr_list = parse_table(query_table_file)
-	print_summary_table(reference_table_dict, reference_chr_list, query_table_dict, query_chr_list, summary_table_file)
-	print_location_table(reference_table_dict, query_table_dict, location_table_file)
+	top_chr_dict = print_summary_table(reference_table_dict, reference_chr_list, query_table_dict, query_chr_list, summary_table_file)
+	print_location_table(reference_table_dict, query_table_dict, location_table_file, top_chr_dict)
 	print("[+] Written output files successfully:")
 	print("[+]	" + summary_table_file)
 	print("[+]	" + location_table_file)
